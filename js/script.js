e8784eca2a2531eaaa9602809d769932245ab36f
Anmol Singh <166452068+Anmol1l@users.noreply.github.com>
2026-08-08 18:01:16 +0530

    Make gameboard to IIFE factory and Inherit its methods to players
    board is now private and its values can be accessed with helper methods inside the factory

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
-const gameboard = {
-    gameboard: [
+const gameboard = ( () => {
+    
+    const gameboard =  [
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0],
-    ],
-}
+    ];
+
+    const getGameboard = () => gameboard;
+    
+    const setMarkPrivate = (playerMark,x,y) => {
+        gameboard[x][y] = playerMark;
+    };
+
+    const setMarkInherit = () => setMarkPrivate;
+
+    return {getGameboard, setMarkInherit}
+
+}) ();
 
  function createPlayer(name,mark) {
+    const {getGameboard, setMarkInherit} = gameboard;
+
     name = name;
     mark = mark;
     let status = false;
     const getStatus = () => status;
     const changeStatus = () => { status = true };
 
-    return {name, mark, getStatus, changeStatus};
+    const setMark = (x,y) => {
+        setMarkInherit()(mark,x,y);
+    };
+
+    return { name, mark, getStatus, changeStatus, getGameboard, setMark};
  }
 
  const player1 = createPlayer("blue", 1);
