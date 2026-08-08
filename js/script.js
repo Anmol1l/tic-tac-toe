270b4d9c7ef957314041f57285232b87620cd8d3
Anmol Singh <166452068+Anmol1l@users.noreply.github.com>
2026-08-08 23:27:07 +0530

    Add contraints to setMark so that
    1. It will not rewrite input;
    2. Player cannot perform consecutive turns

--- js/script.js
+++ js/script.js
@@ -1,35 +1,50 @@
 const gameboard = (() => {
 
     const board = [
-        [2, 0, 0],
+        [1, 0, 0],
+        [0, 1, 0],
         [0, 0, 0],
-        [1, 1, 0],
     ];
+
+    let lastMark = 0;
+
+    const getLastMark = () => lastMark;
 
     const getBoard = () => board;
 
     const setMarkPrivate = (playerMark, x, y) => {
+        lastMark = playerMark
         board[x][y] = playerMark;
     };
 
     const setMarkInherit = () => setMarkPrivate;
 
-    return { getBoard, setMarkInherit }
+    return { getBoard, setMarkInherit, getLastMark }
 
 })();
 
 function createPlayer(name, mark) {
-    const { getBoard, setMarkInherit } = gameboard;
+    const { getBoard, setMarkInherit, getLastMark } = gameboard;
 
     name = name;
     mark = mark;
     let status = false;
     const getStatus = () => status;
-    const changeStatus = () => { status = true };
+    const changeStatus = () => { status = true }; 
 
     const setMark = (x, y) => {
-        setMarkInherit()(mark, x, y);
-        win();
+
+        if (!getBoard()[x][y] && getLastMark() != mark){
+            setMarkInherit()(mark, x, y);
+            checkWin();
+            console.log(getBoard())
+        }
+        else if(getBoard()[x][y] != 0) {
+            console.log("Occupied")
+        }
+        else if(getLastMark() == mark) {
+            console.log("Not Your Turn")
+        }
     };
 
     return { name, mark, getStatus, changeStatus, getBoard, setMark };
@@ -38,19 +53,13 @@ function createPlayer(name, mark) {
 const player1 = createPlayer("blue", 1);
 const player2 = createPlayer("red", 2);
 
-const winCombos = {
-
-}
-
-
-
 function checkWin() {
-    const {getBoard} = gameboard;
+    const { getBoard } = gameboard;
 
     const row1 = getBoard()[0][0] && getBoard()[0][1] && getBoard()[0][2];
     const row2 = getBoard()[1][0] && getBoard()[1][1] && getBoard()[1][2];
     const row3 = getBoard()[2][0] && getBoard()[2][1] && getBoard()[2][2];
-    
+
     const col1 = getBoard()[0][0] && getBoard()[1][0] && getBoard()[2][0];
     const col2 = getBoard()[1][1] && getBoard()[1][1] && getBoard()[1][2];
     const col3 = getBoard()[2][0] && getBoard()[2][1] && getBoard()[2][2];
@@ -59,12 +68,10 @@ function checkWin() {
     const diag2 = getBoard()[0][2] && getBoard()[1][1] && getBoard()[2][0];
 
 
-    if ((row1 || row2 || row3 || col1 || col2 || col3 || diag1 || diag2) === 1) {
+    if (row1 == 1 || row2 == 1 || row3 == 1 || col1 == 1 || col2 ==1 || col3 ==1 || diag1 == 1 || diag2 == 1) {
         console.log("1 win")
     }
-    if ((row1 || row2 || row3 || col1 || col2 || col3 || diag1 || diag2)=== 2) {
+    if (row1 == 2 || row2 == 2 || row3 == 2 || col1 == 2 || col2 == 2 || col3 == 2 || diag1 == 2 || diag2 == 2) {
         console.log("2 win")
     }
-
-
 }
