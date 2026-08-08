9949832a937a5ba905d87a84a932de79c40c9ccf
Anmol Singh <166452068+Anmol1l@users.noreply.github.com>
2026-08-09 00:35:45 +0530

    Fixed wrong logic in checkWin

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ function createPlayer(name, mark) {
 
         if (!getBoard()[x][y] && getLastMark() != mark){
             setMarkInherit()(mark, x, y);
-            checkWin();
+            checkWin(mark);
             console.log(getBoard())
         }
         else if(getBoard()[x][y] != 0) {
@@ -53,25 +53,25 @@ function createPlayer(name, mark) {
 const player1 = createPlayer("blue", 1);
 const player2 = createPlayer("red", 2);
 
-function checkWin() {
+function checkWin(playerMark) {
     const { getBoard } = gameboard;
+    
+    const row1 = ((getBoard()[0][0] == getBoard()[0][1]) && (getBoard()[0][1] == getBoard()[0][2])) && (getBoard()[0][2] == playerMark);
+    const row2 = ((getBoard()[1][0] == getBoard()[1][1]) && (getBoard()[1][1] == getBoard()[1][2])) && (getBoard()[1][2] == playerMark);
+    const row3 = ((getBoard()[2][0] == getBoard()[2][1]) && (getBoard()[2][1] == getBoard()[2][2])) && (getBoard()[2][2] == playerMark);
+    
+    const col1 = ((getBoard()[0][0] == getBoard()[1][0]) && (getBoard()[1][0] == getBoard()[2][0])) && (getBoard()[2][0] == playerMark);
+    const col2 = ((getBoard()[0][1] == getBoard()[1][1]) && (getBoard()[1][1] == getBoard()[2][1])) && (getBoard()[2][1] == playerMark);
+    const col3 = ((getBoard()[0][2] == getBoard()[1][2]) && (getBoard()[1][2] == getBoard()[2][2])) && (getBoard()[2][2] == playerMark);
 
-    const row1 = getBoard()[0][0] && getBoard()[0][1] && getBoard()[0][2];
-    const row2 = getBoard()[1][0] && getBoard()[1][1] && getBoard()[1][2];
-    const row3 = getBoard()[2][0] && getBoard()[2][1] && getBoard()[2][2];
-
-    const col1 = getBoard()[0][0] && getBoard()[1][0] && getBoard()[2][0];
-    const col2 = getBoard()[1][1] && getBoard()[1][1] && getBoard()[1][2];
-    const col3 = getBoard()[2][0] && getBoard()[2][1] && getBoard()[2][2];
-
-    const diag1 = getBoard()[0][0] && getBoard()[1][1] && getBoard()[2][2];
-    const diag2 = getBoard()[0][2] && getBoard()[1][1] && getBoard()[2][0];
+    const diag1 = ((getBoard()[0][0] == getBoard()[1][1]) && (getBoard()[1][1] == getBoard()[2][2])) && (getBoard()[2][2] == playerMark);
+    const diag2 = ((getBoard()[0][2] == getBoard()[1][1]) && (getBoard()[1][1] == getBoard()[2][0])) && (getBoard()[2][0] == playerMark);
 
 
-    if (row1 == 1 || row2 == 1 || row3 == 1 || col1 == 1 || col2 ==1 || col3 ==1 || diag1 == 1 || diag2 == 1) {
+    if ((row1 || row2 || row3 || col1 || col2 || col3 || diag1 || diag2) == 1) {
         console.log("1 win")
     }
-    if (row1 == 2 || row2 == 2 || row3 == 2 || col1 == 2 || col2 == 2 || col3 == 2 || diag1 == 2 || diag2 == 2) {
+    if ((row1 || row2 || row3 || col1 || col2 || col3 || diag1 || diag2) == 2) {
         console.log("2 win")
     }
 }
